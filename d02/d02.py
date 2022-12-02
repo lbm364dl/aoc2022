1c14c9389e4ea7db92f00b5dfc1b039781dd8aca
lbm364dl <geometrydashm364@gmail.com>
2022-12-02 18:15:46 +0100

    even shorter and more unreadable

--- d02/d02.py
+++ d02/d02.py
@@ -1,1 +1,1 @@
-print(*map(sum,zip(*[(lambda a,b:((b-a+1)%3*3+b,(a+b)%3-2+b*3))(ord(a)-64,ord(b)-87)for a,b in map(str.split,open(0))])))
+print(*map(sum,zip(*[(((b:=ord(d)-87)-(a:=ord(c)-64)+1)%3*3+b,(a+b)%3-2+b*3)for c,d in map(str.split,open(0))])))
